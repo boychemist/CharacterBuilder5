af50930b6204f2ea77c25087fa55a09ee46d56ff
George <boychemist1@gmail.com>
2018-12-04 15:54:03 -0800

    Reformat and remove unused imports from a number of files.

--- src/main/scala/org/boychemist/characterbuilder5/DndBuilder.scala
+++ src/main/scala/org/boychemist/characterbuilder5/DndBuilder.scala
@@ -1,16 +1,16 @@
 package org.boychemist.characterbuilder5
 
 import slick.jdbc.JdbcBackend.Database
-import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
-import scalafx.event.EventTarget
 import ui.BuilderUI
 
-object DndBuilder extends JFXApp  {
+object DndBuilder extends JFXApp {
 
-  val db = Database.forURL("jdbc:h2:~/character5", "sa", keepAliveConnection = true,
-    driver = "org.h2.Driver")
+  val db = Database.forURL("jdbc:h2:~/character5",
+                           "sa",
+                           keepAliveConnection = true,
+                           driver = "org.h2.Driver")
 
   stage = new PrimaryStage {
     title = "Dungeons and Dragons V5 Character Builder"
